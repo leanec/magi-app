afd964a7b4fede812799085af1bcfa3f80a0cdd1
leanec <lcorsino@zettasys.com.ar>
2020-09-15 12:09:37 -0300

    Eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es2017: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "airbnb-base",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaVersion: 2017,
+    sourceType: "module",
+  },
+  plugins: [
+    "vue",
+  ],
+  rules: {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "import/extensions": "off",
+    "max-len": "off",
+    "import/no-unresolved": "off",
+    "vue/attribute-hyphenation": ["error", "never"],
+    "vue/max-attributes-per-line": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+};
